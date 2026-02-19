fb0d69d8ee7198998e12c3cafe5533df919d32d2
Brendan Marshall <bmarshall@brendans-air.mynetworksettings.com>
2026-02-19 16:55:52 -0500

    commit

--- utils/task_registry.py
+++ utils/task_registry.py
@@ -6,24 +6,31 @@ from utils.logger import logger
 
 def register_tasks(runner_class):
     integration_name = runner_class.name
-    integration_dir = os.path.dirname(runner_class.__module__.replace(".", "/"))
 
     for subfolder in ["collectors", "insights"]:
-        task_type = subfolder
-        task_path = os.path.join(os.path.dirname(__file__), "..", integration_dir, subfolder)
+        task_path = os.path.join(
+            os.path.dirname(__file__),  # /worker/utils
+            "..",                        # /worker
+            "integrations",              # /worker/integrations
+            integration_name,            # /worker/integrations/hello_world
+            subfolder                    # /worker/integrations/hello_world/collectors
+        )
         task_path = os.path.abspath(task_path)
-        package = f"integrations.{integration_name}.{subfolder}"
 
         if not os.path.isdir(task_path):
             logger.warning(f"No directory found for: {task_path}")
             continue
 
         for _, module_name, _ in pkgutil.iter_modules([task_path]):
-            full_module = f"{package}.{module_name}"
+            full_module = f"{module_name}"
             try:
-                module = importlib.import_module(full_module)
+                import importlib.util
+                file_path = os.path.join(task_path, f"{module_name}.py")
+                spec = importlib.util.spec_from_file_location(module_name, file_path)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
             except Exception as e:
-                logger.error(f"Failed to import {full_module}: {e}")
+                logger.error(f"Failed to import {module_name}: {e}")
                 continue
 
             for attr_name in dir(module):
